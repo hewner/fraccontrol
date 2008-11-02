b80f185413a8e2492dd92039bcc34b1ca42b9104
hewner <hewner@8efd15b0-a470-11dd-91bf-632da8b6e10b>
2008-11-02 22:24:36 +0000

    New version using designs and knowing when to stop.

--- FracControl/FractalPainter.java
+++ FracControl/FractalPainter.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 public class FractalPainter {
 	protected BufferedImage image;
-	protected Map<String, Rule> rules;
-	protected String startRule;
+	protected Map<String, Design> designs;
+	protected String startDesign;
 	
 	public class RenderingException extends Exception {
 		public RenderingException(String error) {
@@ -21,24 +21,24 @@ public class FractalPainter {
 	
 	public FractalPainter()
 	{
-		rules = new HashMap<String,Rule>();
+		designs = new HashMap<String,Design>();
 	}
 	
 	public void startDrawingWithSize(int width, int height) throws RenderingException {
 		System.out.println("width " + width + " height " + height);
 		if(width <= 0) throw new RenderingException("Width " + width);
 		if(height <= 0) throw new RenderingException("Height " + height);
-		if(startRule == null) throw new RenderingException("No start rule!");
+		if(startDesign == null) throw new RenderingException("No start rule!");
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D graphics = image.createGraphics();
 		startDrawing(graphics);
 	}
 	protected class Pair {
 		public Graphics2D g;
-		public Rule rule;
-		public Pair(Graphics2D g, Rule rule) {
+		public Design design;
+		public Pair(Graphics2D g, Design design) {
 			this.g = g;
-			this.rule = rule;
+			this.design = design;
 		}
 	}
 	
@@ -46,33 +46,29 @@ public class FractalPainter {
 	protected class PaintThread extends Thread {
 		
 		public boolean shouldStop = false;
-		public PaintThread(Rule rule, Graphics2D g) {
+		public FractalPainter painter;
+		
+		public PaintThread(Design design, Graphics2D g, FractalPainter painter) {
 			toDraw = new LinkedList<Pair>();
-			toDraw.add(new Pair(g, rule));
+			toDraw.add(new Pair(g, design));
+			this.painter = painter;
 		}
-/*		public void drawSpiral(Graphics2D g2) {
-			for(int i = 0; i < 1000; i++) {
-				g2.fillRect(0, 0, 12, 20);
-				g2.scale(.995, .995);
-				g2.rotate(.05);
-				g2.translate(12,0);
-				if(i % 50 == 49) {
-					Graphics2D newBranch = (Graphics2D) g2.create();
-					newBranch.rotate(Math.PI);
-					newBranch.translate(0, -20);
-					toDraw.add(newBranch);
-				}
-			
-			}			
-		}*/
+
 		public void shouldStop() {
 			shouldStop = true;
 		}
 		public void run() {
 			while(!toDraw.isEmpty() && !shouldStop) {
 				Pair current = toDraw.remove();
-				current.rule.doDraw(current.g);
-			}		
+				if(isTooSmall(current.g)) {
+					//System.out.println("Stopping recurse.  Too small.");
+				} else {
+					current.design.draw(current.g,painter);
+				}
+			}
+			if(!shouldStop) {
+				System.out.println("Finished drawing.");
+			}
 		}
 		
 	}
@@ -83,43 +79,54 @@ public class FractalPainter {
 			while(thread.isAlive()) {} //ensure we have only 1 thread processing
 		}
 		g.setColor(Color.WHITE);
-		g.translate(100, 100);
+		g.scale(unitLength(), unitLength());
+		g.translate(.05, .05);
 		
 		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, 
 				RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHints(rh);
 		
-		thread = new PaintThread(getRule(startRule), g);
+		thread = new PaintThread(getDesign(startDesign), g, this);
 		thread.start();
 	}
 	
-	public synchronized Rule getRule(String name) throws RenderingException {
-		if(!rules.containsKey(name)) {
-			throw new RenderingException("Reference to unknown rule " + name);
+	public synchronized Design getDesign(String name) throws RenderingException {
+		if(!designs.containsKey(name)) {
+			throw new RenderingException("Reference to unknown design " + name);
 		}
-		return rules.get(name);
+		return designs.get(name);
 	}
 	
 	public void drawCurrentImage(Graphics g) {
 			g.drawImage(image, 0,0,Color.BLACK, null);
 	}
 
-	public void addRule(String name, Rule rule) {
-		rule.setPainter(this);
-		rules.put(name, rule);
+	public void addDesign(String name, Design design) {
+		designs.put(name, design);
 	}
 
 	public void setStartRule(String name) {
-		startRule = name;
+		startDesign = name;
 		
 	}
 
-	public static final int maxRules = 100;
+	public static final int maxRules = 10000;
 	public synchronized void  addTask(String name, Graphics2D newG) throws RenderingException {
 		if(toDraw.size() < maxRules) {
-			toDraw.add(new Pair(newG, getRule(name)));
+			toDraw.add(new Pair(newG, getDesign(name)));
 		} else {
 			System.err.println("Exceeded toDraw max!");
 		}
+	}
+	
+	public double unitLength() {
+		int height = image.getHeight();
+		int width = image.getWidth();
+		int min = height > width ? width : height;
+		return .90*min;
+	}
+	
+	public boolean isTooSmall(Graphics2D g) {
+		return(g.getTransform().getScaleX() < .5);
 	}
 }
