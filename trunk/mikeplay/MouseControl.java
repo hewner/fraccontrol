d352c887986033e1dde30690c704b9538f6524a3
hewner <hewner@8efd15b0-a470-11dd-91bf-632da8b6e10b>
2008-11-16 00:06:29 +0000

    fancier menu

--- trunk/mikeplay/MouseControl.java
+++ trunk/mikeplay/MouseControl.java
@@ -66,10 +66,29 @@ public class MouseControl implements MouseListener, MouseMotionListener, KeyList
 	public void keyTyped(KeyEvent e) {
 	
 		if(e.getKeyChar() == 'a') {
-			artist.decrementTemplate();
+			if(artist.getMenuColumn() == 0) {
+				artist.decrementCurrentDesignCategory();
+			}
+			if(artist.getMenuColumn() == 1) {
+				artist.decrementCurrentDesign();
+			}
+			if(artist.getMenuColumn() == 2) {
+				artist.decrementTemplate();
+			}
+		}
+		if(e.getKeyChar() == 'i') {
+			artist.makeNewDesign();
 		}
 		if(e.getKeyChar() == 'z') {
-			artist.incrementTemplate();
+			if(artist.getMenuColumn() == 0) {
+				artist.incrementCurrentDesignCategory();
+			}
+			if(artist.getMenuColumn() == 1) {
+				artist.incrementCurrentDesign();
+			}
+			if(artist.getMenuColumn() == 2) {
+				artist.incrementTemplate();
+			}		
 		}
 		if(e.getKeyChar() == 'q') {
 			artist.toggleRuleMenu();
@@ -91,6 +110,12 @@ public class MouseControl implements MouseListener, MouseMotionListener, KeyList
 		}
 		if(e.getKeyChar() == 'm') {
 			artist.setCurrentDesign(DesignTemplateLibrary.library().getRandomDesign(DesignTemplateLibrary.library().getTemplate("circle")));
+		}
+		if(e.getKeyChar() == 'p') {
+			artist.setMenuColumn(artist.getMenuColumn() + 1);
+		}
+		if(e.getKeyChar() == 'o') {
+			artist.setMenuColumn(artist.getMenuColumn() - 1);
 		}
 
 	}
